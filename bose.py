f6844c0af3f23f240e3c8762a58d82913f887b2c
gpchn <Gprogrammer@163.com>
2023-09-15 23:55:23 +0800

    ...

--- bose.py
+++ bose.py
@@ -1,1 +1,12 @@
+from string import ascii_lowercase as letters
 
+while True:
+    text = input("text: ").lower()
+    num = 0
+    result = ""
+
+    for i in text:
+        if num == 27:
+            num = 1
+        result += letters[letters.index(i) + num]
+        num += 1
